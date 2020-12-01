28758d44a9a313f4e14ae6045cbcfee03ab715cd
running <627882647@qq.com>
2020-12-01 16:22:38 +0800

    Initial commit

---
+++ src/com/enjoyican/FirstTest.java
@@ -0,0 +1,21 @@
+package com.enjoyican;
+
+import java.util.ArrayList;
+
+/**
+ * TODO
+ *
+ * @author running
+ * @version v1.1
+ * @since 2020/12/1 15:44
+ */
+public class FirstTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("tom");
+        list.add("amy");
+        list.add("okr");
+    }
+
+}
