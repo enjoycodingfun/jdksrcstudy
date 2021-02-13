0e9e22bebe53d03e1a73870a877fe6672ebd6882
running <627882647@qq.com>
2021-02-13 23:53:04 +0800

    hashmap源码复习

--- relsource/java/util/HashMap.java
+++ relsource/java/util/HashMap.java
@@ -629,29 +629,37 @@ public class HashMap<K,V> extends AbstractMap<K,V>
      * @param evict if false, the table is in creation mode.
      * @return previous value, or null if none
      */
+    // 第四个参数 onlyIfAbsent 如果是 true，那么只有在不存在该 key 时才会进行 put 操作
     final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
                    boolean evict) {
         Node<K,V>[] tab; Node<K,V> p; int n, i;
         // 步骤①：tab为空则创建
+        // 第一次 put 值的时候，会触发下面的 resize()，类似 java7 的第一次 put 也要初始化数组长度
+        // 第一次 resize 和后续的扩容有些不一样，因为这次是数组从 null 初始化到默认的 16 或自定义的初始容量
         if ((tab = table) == null || (n = tab.length) == 0)
             n = (tab = resize()).length;
         // 步骤②：计算index，并对null做处理
+        // 找到具体的数组下标，如果此位置没有值，那么直接初始化一下 Node 并放置在这个位置就可以了
         if ((p = tab[i = (n - 1) & hash]) == null)
             tab[i] = newNode(hash, key, value, null);
-        else {
+        else {// 数组该位置有数据
             Node<K,V> e; K k;
             // 步骤③：节点key存在，直接覆盖value
+            // 首先，判断该位置的第一个数据和我们要插入的数据，key 是不是"相等"，如果是，取出这个节点
             if (p.hash == hash &&
                 ((k = p.key) == key || (key != null && key.equals(k))))
                 e = p;
-                // 步骤④：判断该链为红黑树
+                // 步骤④：判断该链为红黑树，如果该节点是代表红黑树的节点，调用红黑树的插值方法
             else if (p instanceof TreeNode)
                 e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
             else {
                 // 步骤⑤：该链为链表
                 for (int binCount = 0; ; ++binCount) {
+                    // 插入到链表的最后面(Java7 是插入到链表的最前面)
                     if ((e = p.next) == null) {
                         p.next = newNode(hash, key, value, null);
+                        // TREEIFY_THRESHOLD 为 8，所以，如果新插入的值是链表中的第 8 个
+                        // 会触发下面的 treeifyBin，也就是将链表转换为红黑树
                         if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
                             //链表长度大于8转换为红黑树进行处理
                             treeifyBin(tab, hash);
@@ -660,10 +668,13 @@ public class HashMap<K,V> extends AbstractMap<K,V>
                     // key已经存在直接覆盖value
                     if (e.hash == hash &&
                         ((k = e.key) == key || (key != null && key.equals(k))))
+                        // 此时 break，那么 e 为链表中[与要插入的新值的 key "相等"]的 node
                         break;
                     p = e;
                 }
             }
+            // e!=null 说明存在旧值的key与要插入的key"相等"
+            // 对于我们分析的put操作，下面这个 if 其实就是进行 "值覆盖"，然后返回旧值
             if (e != null) { // existing mapping for key
                 V oldValue = e.value;
                 if (!onlyIfAbsent || oldValue == null)
@@ -701,13 +712,17 @@ public class HashMap<K,V> extends AbstractMap<K,V>
                 threshold = Integer.MAX_VALUE;
                 return oldTab;
             }
+            // 将数组大小扩大一倍
             else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY &&
                      oldCap >= DEFAULT_INITIAL_CAPACITY)
+                // 将阈值扩大一倍
                 newThr = oldThr << 1; // double threshold
         }
+        // 对应使用 new HashMap(int initialCapacity) 初始化后，第一次 put 的时候
         else if (oldThr > 0) // initial capacity was placed in threshold
             newCap = oldThr;
         else {               // zero initial threshold signifies using defaults
+            // 对应使用 new HashMap() 初始化后，第一次 put 的时候
             newCap = DEFAULT_INITIAL_CAPACITY;
             newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
         }
@@ -718,6 +733,7 @@ public class HashMap<K,V> extends AbstractMap<K,V>
         }
         threshold = newThr;
         @SuppressWarnings({"rawtypes","unchecked"})
+        // 用新的数组大小初始化新的数组
             Node<K,V>[] newTab = (Node<K,V>[])new Node[newCap];
         table = newTab;
         if (oldTab != null) {
